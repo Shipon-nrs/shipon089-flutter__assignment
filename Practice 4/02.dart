b5a31bf827c48a9caa10d9eb5fb579b4619ab51c
Shipon-nrs <md.rahman3898@gmail.com>
2025-10-18 13:55:14 +0600

    Add fruit set and print loop in 02.dart
    This program creates a set of fruits and prints each fruit using a loop.

---
+++ Practice 4/02.dart
@@ -0,0 +1,7 @@
+/*2. Create a set of fruits and print all fruits using loop.*/
+void main() {
+  var fruits = {"Apple", "Banana", "Mango", "Orange", "Grapes"};
+  for (var fruit in fruits) {
+    print(fruit);
+  }
+}
